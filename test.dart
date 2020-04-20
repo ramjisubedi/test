3db30e52477768912af2e74637603dc935c53178
ramjisubedi <ramjisubedi1991@gmail.com>
2020-04-20 21:42:21 +0545

    Update test.dart

--- test.dart
+++ test.dart
@@ -1,57 +1,136 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:med/page/zoom_page.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-class PrescriptionPage extends StatefulWidget {
-  final String title;
-  PrescriptionPage(this.title);
-  bool isSelected = false;
-  
+
+class HomePage extends StatefulWidget {
   @override
-  _PrescriptionPageState createState() => _PrescriptionPageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _PrescriptionPageState extends State<PrescriptionPage> {
-  bool isSelected = false;
- List<Item> selectedList;
-  Future<List> refresh() async{
+class _HomePageState extends State<HomePage> {
+  List<Item> itemList;
+  List<Item> selectedList;
+Future<List> refresh() async{
  var url = 'http://med.ramjisubedi.com.np/get.php';
  final response = await http.get(url);
- return json.decode(response.body);
-
+ //return json.decode(response.body);
+ var data = json.decode(response.body);
+itemList = List();
+selectedList = List();
+for(var u in data){
+  itemList.add(Item(u['image'],u['id']));
 }
+print(itemList.length);
+}
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-       ),
-            body: RefreshIndicator(
-              child: FutureBuilder(
-                future: refresh(),
-                builder: (context,snap){
-                if(!snap.hasData)
+      appBar: getAppBar(),
+      body: FutureBuilder(
+        future: refresh(),
+        builder: (context,snap){
+          if(snap.data==null)
                 {
+                  print(snap.data);
                   return Center(
                     child: CircularProgressIndicator(),
                   );
                 }
+GridView.builder(
+          itemCount: itemList.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: 0.56,
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2),
+          itemBuilder: (context, index) {
+            return GridItem(
+                item: itemList[index],
+                isSelected: (bool value) {
+                  setState(() {
+                    if (value) {
+                      selectedList.add(itemList[index]);
+                    } else {
+                      selectedList.remove(itemList[index]);
+                    }
+                  });
+                  print("$index : $value");
+                },
+                key: Key(itemList[index].rank.toString()));
+                
+          });
+      })
+    );
+  }
 
-        return GestureDetector(
-child:  GridView.extent(maxCrossAxisExtent: 120.0,
-        children: List.generate(snap.data.length, (index){
-         print(snap.data[index]);
-return GestureDetector(
-  child: Card(
- child:Stack(
-children:<Widget>[ 
-  CachedNetworkImage(
-            placeholder: (context, url) => CircularProgressIndicator(),
-            imageUrl:
-                'http://med.ramjisubedi.com.np/uploads/'+snap.data[index]['image'],
-                fit: BoxFit.fill,
-   colorBlendMode: BlendMode.modulate,
+  getAppBar() {
+    return AppBar(
+      title: Text(selectedList.length < 1
+          ? "Multi Selection"
+          : "${selectedList.length} item selected"),
+      actions: <Widget>[
+        selectedList.length < 1
+            ? Container()
+            : InkWell(
+                onTap: () {
+                  setState(() {
+                    for (int i = 0; i < selectedList.length; i++) {
+                      itemList.remove(selectedList[i]);
+                    }
+                    selectedList = List();
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.delete),
+                ))
+      ],
+    );
+  }
+}
+
+class Item {
+  String imageUrl;
+  int rank;
+
+  Item(this.imageUrl, this.rank);
+}
+
+class GridItem extends StatefulWidget {
+  final Key key;
+  final Item item;
+  final ValueChanged<bool> isSelected;
+
+  GridItem({this.item, this.isSelected, this.key});
+
+  @override
+  _GridItemState createState() => _GridItemState();
+}
+
+class _GridItemState extends State<GridItem> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+          widget.isSelected(isSelected);
+        });
+      },
+      child: Stack(
+        children: <Widget>[
+          Image.asset(
+            widget.item.imageUrl,
+            color: Colors.black.withOpacity(isSelected ? 0.9 : 0),
+            colorBlendMode: BlendMode.color,
           ),
           isSelected
               ? Align(
@@ -65,67 +144,8 @@ children:<Widget>[
             ),
           )
               : Container()
-          ],
-)
-),
- onTap: () {
- Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                        new ZoomPage('http://med.ramjisubedi.com.np/uploads/'+snap.data[index]['image'])   
-                        ));
-         },
-         
-         onLongPress: (){
-           setState(() {
-          isSelected = !isSelected;
-        });
-          print('long press');
-         },
-);
-
-        }
-        ),
-        ),
-      
-        );
-       
-              })
-            
-            , onRefresh: refresh),
-          );
-       
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  final String title;
-  DetailScreen(this.title);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        child: Center(
-          child: Hero(
-            tag: 'imageHero',
-            child: Image.network(
-              title,
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.pop(context);
-        },
+        ],
       ),
     );
   }
 }
-class Item {
-  String imageUrl;
-  int rank;
-  String text;
-
-  Item(this.rank,this.text,this.imageUrl);
-}
-
-
-
